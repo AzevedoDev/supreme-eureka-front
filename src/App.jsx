ff2e8d90057d943daa609ac65d42d4eedd278287
Pedro Azevedo <theazevedo@outlook.com>
2022-04-03 02:22:32 -0300

    add more info into table

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,111 @@
 import results from './results-1648956529971.json';
 import './App.css';
 function App() {
-  console.table(results);
   return (
     <div className="w-full h-auto bg-slate-400 flex flex-col items-center">
       <header className="App-header">
         <h1 className="">Supreme Eureka Front</h1>
       </header>
       <div>
-        {results.map(({ position, username, rating, matches }) => {
-          return (
-            <div key={position} className="py-2 px-2 bg-white rounded-lg my-4">
-              <div className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <h2 className="px-6 py-4 font-medium text-lg text-gray-900 dark:text-white whitespace-nowrap">{`${position} - ${username}`}</h2>
-                <p>Rating: {rating}</p>
-              </div>
-              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                {matches.map(
-                  ({ opponent, opponentDeck, result, type, replay }) => {
-                    return (
-                      <>
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                          <tr className="content-center">
-                            <th scope="col" className="px-6 py-3">
-                              Opponent
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                              Opponent Deck
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                              Result
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                              Game Type
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                              Replay
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                          <td
-                            scope="row"
-                            className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+        {results.map(
+          ({
+            position,
+            username,
+            rating,
+            matches,
+            wins,
+            loses,
+            lastUpdate,
+          }) => {
+            return (
+              <div
+                key={position}
+                className="py-2 px-2 bg-white rounded-lg my-4"
+              >
+                <div className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <h2 className="px-6 py-4 font-medium text-lg text-gray-900 dark:text-white whitespace-nowrap">{`${position} - ${username}`}</h2>
+                  <div className="flex w-full justify-evenly">
+                    <p className="px-6 font-medium text-lg text-gray-900 dark:text-white">
+                      Rating: {rating}
+                    </p>
+                    <p className="px-6 font-medium text-lg text-gray-900 dark:text-white">
+                      Wins: {wins}
+                    </p>
+                    <p className="px-6 font-medium text-lg text-gray-900 dark:text-white">
+                      Loses: {loses}
+                    </p>{' '}
+                    <p className="px-6 font-medium text-lg text-gray-900 dark:text-white">
+                      Last Update: {new Date(lastUpdate).toLocaleDateString()}
+                    </p>
+                  </div>
+                  <p className="px-6 py-4 font-medium text-lg text-gray-900 dark:text-white flex justify-center">
+                    Replays
+                  </p>
+                </div>
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                  {matches.map(
+                    (
+                      { opponent, opponentDeck, result, type, replay, date },
+                      index
+                    ) => {
+                      return (
+                        <>
+                          <thead
+                            key={index}
+                            className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
                           >
-                            {opponent}
-                          </td>
-                          <td className="px-6 py-4">{opponentDeck}</td>
-                          <td className="px-6 py-4">{result}</td>
-                          <td className="px-6 py-4">{type}</td>
-                          <td>
-                            <a
-                              href={replay}
-                              className="bg-slate-600 border-1 text-white w-48 h-12 p-2 rounded-full hover:bg-slate-700"
+                            <tr className="content-center">
+                              <th scope="col" className="px-6 py-3">
+                                Opponent
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                Opponent Deck
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                Result
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                Game Type
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                Date
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                Replay
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td
+                              scope="row"
+                              className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
                             >
-                              Go to duel
-                            </a>
-                          </td>
-                        </tbody>
-                      </>
-                    );
-                  }
-                )}
-              </table>
-            </div>
-          );
-        })}
+                              {opponent}
+                            </td>
+                            <td className="px-6 py-4">{opponentDeck}</td>
+                            <td className="px-6 py-4">{result}</td>
+                            <td className="px-6 py-4">{type}</td>
+                            <td className="px-6 py-4">
+                              {date.replaceAll('-', '/')}
+                            </td>
+                            <td>
+                              <a
+                                href={replay}
+                                className="bg-slate-600 border-1 text-white w-48 h-12 p-2 rounded-full hover:bg-slate-700"
+                              >
+                                Go to duel
+                              </a>
+                            </td>
+                          </tbody>
+                        </>
+                      );
+                    }
+                  )}
+                </table>
+              </div>
+            );
+          }
+        )}
       </div>
     </div>
   );
